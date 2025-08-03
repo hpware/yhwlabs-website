d97a79b55ee4e75195a828e24a87cc54b999ca96
吳元皓 <hw@yuanhau.com>
2025-08-03 21:39:31 +0800

    Add navbar

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,4 +1,5 @@
 import { getDictionary } from "@/app/[lang]/dictionaries";
+import Layout from "@/components/layout";
 export default async function Page({
   params,
 }: {
@@ -7,15 +8,21 @@ export default async function Page({
   const { lang } = await params;
   const dict = await getDictionary(lang as any);
   return (
-    <div className="font-sans">
-      <div className="absolute inset-0 justify-center text-center flex flex-col">
-        <h1 className="text-4xl text-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-amber-200">
-          {dict.home.hi} {dict.home.work}
-        </h1>
-        <h3 className="text-xl text-bold text-gray-300">
-          {dict.home.learnmoreaboutourwork}
-        </h3>
+    <Layout dict={dict}>
+      <div className="font-sans">
+        <div className="absolute inset-0 justify-center text-center flex flex-col min-h-screen">
+          <h1 className="text-4xl text-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-amber-200">
+            {dict.home.hi} {dict.home.work}
+          </h1>
+          <h3 className="text-xl text-bold text-gray-300">
+            {dict.home.learnmoreaboutourwork}
+          </h3>
+        </div>
+        <div className="min-h-screen"></div>
+        <div className="min-h-1/2">
+          <h2>I do Websites</h2>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
---
+++ components/layout.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from "react";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+export default function Layout({
+  children,
+  dict,
+}: Readonly<{
+  children: React.ReactNode;
+  dict: any;
+}>) {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      {/** Navbar */}
+      <div
+        className={`min-h-12 fixed z-50 inset-x-0 mx-4 border border-gray-300/30 shadow backdrop-blur-lg flex flex-row justify-between text-center transition-all duration-300 ${scrolled ? "mt-5 rounded-4xl" : "mt-2 rounded-xl"}`}
+      >
+        <div className="p-2 mx-2">
+          <div className="flex flex-row items-center justify-center text-center">
+            <span className="text-xl font-bold ml-2">yhwLabs</span>
+          </div>
+        </div>
+        <div className="p-2 mx-2 flex-row items-center justify-center text-center hidden md:flex">
+          <Link href="/" className="ml-2 mr-2">
+            <span>{dict.nav.home}</span>
+          </Link>
+        </div>
+        <div className="p-2 mx-2 flex-row items-center justify-center text-center flex md:hidden">
+          <button>
+            <MenuIcon />
+          </button>
+        </div>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+}
